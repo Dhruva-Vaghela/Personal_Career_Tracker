4d25daed6583d26d46ce53148666d2fc16c2d46b
Dhruva <dhruva@Dhruvas-MacBook-Air.local>
2026-08-21 05:42:17 +0530

    fix Vercel TanStack Start deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-// @ts-ignore
-import netlify from "@netlify/vite-plugin-tanstack-start";
 import path from "node:path";
 
 export default defineConfig({
   server: {
     port: 8080,
   },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   css: {
     transformer: "postcss",
   },
+
   plugins: [
     tanstackStart(),
     react(),
     tailwindcss(),
-    netlify(),
   ],
 });
-
-
-
